e561496a6ced2df4ea7d01503ae91aaac80942e5
marcosdv <marcosdiasvendramini@yahoo.com.br>
2022-06-25 15:14:30 -0300

    Adicionada classe EditoraHelper

--- lib/datasources/local/editora_helper.dart
+++ lib/datasources/local/editora_helper.dart
@@ -1,4 +1,6 @@
+import 'package:aula02_pos_25_06_2022/datasources/local/banco_dados.dart';
 import 'package:aula02_pos_25_06_2022/models/models.dart';
+import 'package:sqflite/sqflite.dart';
 
 class EditoraHelper {
   static const SQL_CREATE = '''
@@ -7,4 +9,9 @@ class EditoraHelper {
       ${Editora.NOME} TEXT
     )
   ''';
+
+  void inserir(Editora editora) async {
+    Database db = await BancoDados().db;
+    await db.insert(Editora.TABELA, editora.toMap());
+  }
 }
--- lib/models/editora.dart
+++ lib/models/editora.dart
@@ -3,8 +3,17 @@ class Editora {
   static const CODIGO = 'codigo';
   static const NOME = 'nome';
 
-  int codigo;
+  int? codigo;
   String nome;
 
-  Editora(this.codigo, this.nome);
+  Editora({this.codigo, required this.nome});
+
+  factory Editora.fromMap(Map map) {
+    return Editora(
+      codigo: int.tryParse(map[CODIGO].toString()),
+      nome: map[NOME]
+    );
+  }
+
+  Map<String, dynamic> toMap() => { CODIGO: codigo, NOME: nome };
 }
