b275e23ea9213dddd5bae8858a87decf57d769c2
marcosdv <marcosdiasvendramini@yahoo.com.br>
2022-06-25 14:44:25 -0300

    Adicionada classe BancoDados

---
+++ lib/datasources/local/banco_dados.dart
@@ -0,0 +1,32 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class BancoDados {
+  BancoDados.internal();
+  static final BancoDados _instance = BancoDados.internal();
+  factory BancoDados() => _instance;
+
+  Database? _db;
+
+  Future<Database> get db async {
+    /*
+    if (_db == null) {
+      _db = await _iniciarDb();
+    }
+    */
+    _db ??= await _iniciarDb();
+    return _db!;
+  }
+
+  Future<Database> _iniciarDb() async {
+    final pathDb = await getDatabasesPath();
+    final caminhoBanco = join(pathDb, 'meus_livros.db');
+
+    return await openDatabase(caminhoBanco, version: 1,
+      onCreate: (Database db, int newVersion) async {
+        await db.execute('CREATE TABLE TbEditora');
+        await db.execute('CREATE TABLE TbLivro');
+      }
+    );
+  }
+}
