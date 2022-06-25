a0c3d0d6e96df02516f48b1743fa91f0da92d537
marcosdv <marcosdiasvendramini@yahoo.com.br>
2022-06-25 16:21:42 -0300

    Criada listagem de Editoras

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:aula02_pos_25_06_2022/datasources/datasources.dart';
+import 'package:aula02_pos_25_06_2022/models/models.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +10,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _editoraHelper = EditoraHelper();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Livros'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: FutureBuilder(
+        future: _editoraHelper.getTodos(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+            case ConnectionState.none:
+              return const CircularProgressIndicator();
+            default:
+              if (snapshot.hasError) {
+                return Text('Erro ao buscar as Editoras! ${snapshot.error}');
+              }
+              return _criarListagem(snapshot.data as List<Editora>);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _criarListagem(List<Editora> lista) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      scrollDirection: Axis.vertical,
+      itemCount: lista.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(lista[index].nome, style: const TextStyle(fontSize: 22),),
+          ),
+        );
+      }
+    );
   }
 }
