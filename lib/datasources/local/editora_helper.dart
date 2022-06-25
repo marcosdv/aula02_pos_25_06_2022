88c13510050e03ba678d6d3817dc1c7633b6b57c
marcosdv <marcosdiasvendramini@yahoo.com.br>
2022-06-25 15:56:59 -0300

    Finalizada classe EditoraHelper

--- lib/datasources/local/editora_helper.dart
+++ lib/datasources/local/editora_helper.dart
@@ -12,6 +12,43 @@ class EditoraHelper {
 
   void inserir(Editora editora) async {
     Database db = await BancoDados().db;
-    await db.insert(Editora.TABELA, editora.toMap());
+    db.insert(Editora.TABELA, editora.toMap());
+  }
+
+  void alterar(Editora editora) async {
+    Database db = await BancoDados().db;
+
+    db.update(
+      Editora.TABELA,
+      editora.toMap(),
+      where: '${Editora.CODIGO} = ?',
+      whereArgs: [editora.codigo]
+    );
+  }
+
+  void excluir(Editora editora) async {
+    Database db = await BancoDados().db;
+
+    db.delete(
+      Editora.TABELA,
+      where: '${Editora.CODIGO} = ?',
+      whereArgs: [editora.codigo]
+    );
+  }
+
+  Future<List<Editora>> getTodos() async {
+    Database db = await BancoDados().db;
+
+    List listaBanco = await db.query(Editora.TABELA);
+
+    return listaBanco.map((item) => Editora.fromMap(item)).toList();
+    /*
+    List<Editora> listaEditora = [];
+    for (int i = 0; i < listaBanco.length; i++) {
+      Editora editora = Editora.fromMap(listaBanco[i]);
+      listaEditora.add(editora);
+    }
+    return listaEditora;
+    */
   }
 }
--- lib/models/livro.dart
+++ lib/models/livro.dart
@@ -1,9 +1,24 @@
 import 'package:aula02_pos_25_06_2022/models/models.dart';
 
 class Livro {
-  int codigo;
+  static const TABELA = 'TbLivro';
+  static const CODIGO = 'codigo';
+  static const NOME = 'nome';
+  static const EDITORA = 'codEditora';
+
+  int? codigo;
   String nome;
   Editora editora;
 
-  Livro(this.codigo, this.nome, this.editora);
+  Livro({this.codigo, required this.nome, required this.editora});
+
+  factory Livro.fromMap(Map mapa, Editora editora) {
+    return Livro(
+      codigo: int.tryParse(mapa[CODIGO].toString()),
+      nome: mapa[NOME],
+      editora: editora
+    );
+  }
+
+  Map<String, dynamic> toMap() => { CODIGO: codigo, NOME: nome, EDITORA: editora.codigo };
 }
